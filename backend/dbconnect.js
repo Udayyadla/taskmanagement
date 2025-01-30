320c09e5bf8fd3a9d80675c7b56a95df0ab392bf
Yadla Uday Kumar <udaykumar3439@gmail.com>
2025-01-30 15:21:16 +0530

    modified .env file

--- backend/dbconnect.js
+++ backend/dbconnect.js
@@ -1,13 +1,11 @@
-import {connect} from 'mongoose'
-import 'dotenv/config.js'
-export const connectToDB=async()=>{
-    try {
-    await connect(process.env.MONGO_URI)
-    console.log('db connection is success')
-        
-    } catch (error) {
-        console.log('db connection failed')
-    }
-    
-
-}
+import { connect } from "mongoose";
+import "dotenv/config.js";
+export const connectToDB = async () => {
+  try {
+    await connect(process.env.MONGO_URI);
+    console.log("db connection is success");
+  } catch (error) {
+    console.log(error.message);
+    console.log("db connection failed");
+  }
+};
